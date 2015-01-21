e320e59532c5852fee169af49a53e1bca47aefe2
notae <notae@me.com>
2015-01-21 22:48:33 +0900

    Exported sub module.

--- src/Control/CPFD/Fuzzy.hs
+++ src/Control/CPFD/Fuzzy.hs
@@ -1,8 +1,10 @@
 module Control.CPFD.Fuzzy
        (
-         module Control.CPFD.Fuzzy.Solver
+         module Data.Fuzzy
+       , module Control.CPFD.Fuzzy.Solver
        , module Control.CPFD.Fuzzy.Primitives
        ) where
 
 import Control.CPFD.Fuzzy.Primitives
 import Control.CPFD.Fuzzy.Solver
+import Data.Fuzzy
